454569f1bc6587e15308c2c61ad4c972c89d19a2
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-15 12:13:11 +0500

    Определение четвертей координатной плоскости

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите значение x:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение y:");
+int y = int.Parse(Console.ReadLine());
+
+if(x > 0 && y > 0) Console.WriteLine("1-я четверть");
+
+if(x < 0 && y > 0) Console.WriteLine("2-ая четверть");
+
+if(x < 0 && y < 0) Console.WriteLine("3-я четверть");
+
+if(x > 0 && y < 0) Console.WriteLine("4-ая четверть");
