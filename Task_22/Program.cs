394e8ecd9370e846f953c80c5d9ce5bde2651390
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-15 13:52:56 +0500

    таблица квадратов

---
+++ Task_22/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N
+
+Console.WriteLine("Введите целое число =>");
+int n = int.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+    if(i % 5 == 0){
+        Console.WriteLine();
+    }
+    Console.Write(i * i + " ");
+}
