45ae483a0cd9e016081dc0cb718404b6523698cc
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-22 12:20:32 +0500

    полиндром рабочий

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -2,9 +2,23 @@
 // вход пятизначное число и проверяет, является ли оно
 // палиндромом.
 
-Console.WriteLine("Введите целое положительное пятизначное число =>");
-int[] n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое пятизначное число =>");
+int number = int.Parse(Console.ReadLine());
 
-    for (int i = 0; i < n.Length / 2; ++i)
-        if (n[i] == n[n.Length - 1 - i]) Console.WriteLine("Число не является палиндромом");
-    Console.WriteLine("Число является палиндромом");
+// 34543
+if(number > 9999 && number < 100000)
+{
+int digit1 = number / 10000; //3
+int digit2 = number / 1000 % 10; //4
+int digit3 = number / 100 % 10; //5
+int digit4 = number / 10 % 10; //4
+int digit5 = number % 10; //3
+
+string isPolyYesOut = $"Число {number} является полиндромом";
+string isPolyNoOut = $"Число {number} является полиндромом";
+Console.WriteLine(digit1 == digit5 && digit2 == digit4 ? isPolyYesOut : isPolyNoOut);
+}
+
+
+
+
