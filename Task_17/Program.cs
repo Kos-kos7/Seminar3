29adff2e82d7f180f712254862857d60af6c0d3d
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-22 11:28:18 +0500

    принимает координаты и выдает четверть плоскости/добавил решение с использование метода

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -2,16 +2,73 @@
 // координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
 // номер четверти плоскости, в которой находится эта точка.
 
+// Console.WriteLine("Введите значение x:");
+// int x = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите значение y:");
+// int y = int.Parse(Console.ReadLine());
+
+// if(x > 0 && y >0);
+
+// if(x > 0 && y > 0) Console.WriteLine("1-я четверть");
+
+// if(x < 0 && y > 0) Console.WriteLine("2-ая четверть");
+
+// if(x < 0 && y < 0) Console.WriteLine("3-я четверть");
+
+// if(x > 0 && y < 0) Console.WriteLine("4-ая четверть");
+
+
 Console.WriteLine("Введите значение x:");
 int x = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите значение y:");
 int y = int.Parse(Console.ReadLine());
 
-if(x > 0 && y > 0) Console.WriteLine("1-я четверть");
+string quarter = Quarter(x, y);
+Console.WriteLine(quarter);
 
-if(x < 0 && y > 0) Console.WriteLine("2-ая четверть");
 
-if(x < 0 && y < 0) Console.WriteLine("3-я четверть");
+string Quarter(int x, int y)
+{
+    string result = string.Empty;
+    if(x > 0 && y > 0)
+    {
+        result = "Координаты относятся к 1-й четверти";
+    }
+    if(x < 0 && y > 0)
+    {
+        result = "Координаты относятся к 2-й четверти";
+    }
+    if(x < 0 && y < 0)
+    {
+        result = "Координаты относятся к 3-й четверти";
+    }
+    if(x > 0 && y < 0)
+    {
+        result = "Координаты относятся к 4-й четверти";
+    }
+    return result;
+}
 
-if(x > 0 && y < 0) Console.WriteLine("4-ая четверть");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -3,22 +3,29 @@
 // палиндромом.
 
 Console.WriteLine("Введите целое пятизначное число =>");
-int number = int.Parse(Console.ReadLine());
+var number = Console.ReadLine();
 
 // 34543
-if(number > 9999 && number < 100000)
-{
-int digit1 = number / 10000; //3
-int digit2 = number / 1000 % 10; //4
-int digit3 = number / 100 % 10; //5
-int digit4 = number / 10 % 10; //4
-int digit5 = number % 10; //3
+// if (number > 9999 && number < 100000)
+// {
+//     int digit1 = number / 10000; //3
+//     int digit2 = number / 1000 % 10; //4
+//     int digit3 = number / 100 % 10; //5
+//     int digit4 = number / 10 % 10; //4
+//     int digit5 = number % 10; //3
 
-string isPolyYesOut = $"Число {number} является полиндромом";
-string isPolyNoOut = $"Число {number} является полиндромом";
-Console.WriteLine(digit1 == digit5 && digit2 == digit4 ? isPolyYesOut : isPolyNoOut);
+//     string isPolyYesOut = $"Число {number} является полиндромом";
+//     string isPolyNoOut = $"Число {number} является полиндромом";
+//     Console.WriteLine(digit1 == digit5 && digit2 == digit4 ? isPolyYesOut : isPolyNoOut);
+// }
+
+//3456543
+
+bool isPolyndrom = true;
+for (int i = 0; i < number.Length; i++)
+{
+    if(number[i] != number [number.Length - 1 - i])
+    isPolyndrom = false;
 }
 
-
-
-
+Console.WriteLine(isPolyndrom);
